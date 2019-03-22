e4c47367ca184cfb8c54c81e72cf36989bc01e03
Hackbright Student <no-reply@hackbrightacademy.com>
2019-03-21 20:57:55 -0700

    Part 1 completed

--- seed.py
+++ seed.py
@@ -66,7 +66,7 @@ def load_ratings():
     Rating.query.delete()
 
     for row in open("seed_data/u.data"):
-        row = row.rstrip().split("|")
+        row = row.rstrip().split()
 
         user_id = row[0]
         movie_id = row[1]
